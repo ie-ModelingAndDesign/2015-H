c27235d05473401ec36ae90bb6baddbdfb3ea326
Saaya YAMASHIRO <w_sacora_w@yahoo.co.jp>
2015-12-22 15:08:06 +0900

    add words

---
+++ Nyusankinmaite/Nyusankinmaite/CopyView.swift
@@ -0,0 +1,9 @@
+//
+//  CopyView.swift
+//  Nyusankinmaite
+//
+//  Created by 山城　沙綾 on 2015/11/27.
+//  Copyright © 2015年 Ryuki. All rights reserved.
+//
+
+import Foundation
---
+++ Nyusankinmaite/Nyusankinmaite/json.swift
@@ -0,0 +1,9 @@
+//
+//  json.swift
+//  Nyusankinmaite
+//
+//  Created by 山城　沙綾 on 2015/12/17.
+//  Copyright © 2015年 Ryuki. All rights reserved.
+//
+
+import Foundation
